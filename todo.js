53b768c6306fff2619b9b10a15c37f53a3e9b5f1
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-21 14:06:15 -0700

    adding appendChild method

--- todo.js
+++ todo.js
@@ -4,9 +4,9 @@ var errorElement = document.getElementById("error");
 function createToDoTask(){
     var textInput = document.getElementById("task").value;
     if (textInput.length > 0) {
-        var textElement = document.createElement("li");
-        var inputChildren = textInput.children;
-        var addChildren = add.Button.children;
+        var listElement = document.createElement("li");
+        var text = document.createTextNode('bla bla bla');
+        listElement.appendChild(text);
     }
     else {
         errorElement.innerHTML = "You have to input something";
