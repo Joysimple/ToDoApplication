900463d3b31c9d0ac2f7ede29454039fd7548a3f
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-21 11:44:19 -0700

    changin function in todo.js

--- todo.js
+++ todo.js
@@ -1,11 +1,14 @@
 var addButton = document.getElementById("add");
 addButton.addEventListener("click", createToDoTask);
-var textInput = document.getElementById("task").value;
+var errorElement = document.getElementById("error");
 function createToDoTask(){
+    var textInput = document.getElementById("task").value;
     if (textInput.length > 0) {
-        var textElement = document.createElement("list");
+        var textElement = document.createElement("li");
+        var inputChildren = textInput.children;
+        var addChildren = add.Button.children;
     }
     else {
-        alert("enter something");
+        errorElement.innerHTML = "You have to input something";
     }
 }
